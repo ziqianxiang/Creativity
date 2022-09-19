f9ae5b21417f85b4a352a7ea191ed419a1ec347e
ziqianxiang <ziqianxiang1220@163.com>
2022-09-19 17:29:03 +0800

    new0919

--- ziqian_python/split_reference_new.py
+++ ziqian_python/split_reference_new.py
@@ -1,4 +1,5 @@
 from argparse import ONE_OR_MORE
+from asyncio.windows_events import NULL
 import os
 import re
 '''提取文档"D:\研究生-心理与行为大数据\ziqianの创造力\ziqian_python\_CfpJazzXT2.txt"中Reference和Appendix之间的文本'''
@@ -34,24 +35,54 @@ for x in newref:
    newreference.append(x.replace("\n", ""))
 #print(newreference)
 
+
+
 '''
 以‘.’分割列表中的每一条参考文献
 '''
+
 newreference2=[]
 for x in newreference:
         newreference2.append(x.split('.'))#以‘.’分割列表中的每一条参考文献
-print(newreference2)
+#print(newreference2)
+
+#提取每一条参考文献中的作者，论文题目，会议名称
+newreference3=[]
+for x in newreference2:
+    listelement=[]
+    if len(x)>=2:
+       listelement.append(x[1])
+    if len(x)>2:
+        x[2].split(',')
+        ref=x[2].split(',')
+        listelement.append(ref[0])
+    if len(listelement)==2:
+        newreference3.append(listelement)
+print(newreference3)
+print(len(newreference3))
+'''提取列表每一项的论文题目以及会议，写入csv文件'''
+import csv
+with open(r"reference3.csv",'w',newline='',encoding='utf-8') as f:
+    writer=csv.writer(f)
+    writer.writerow(['论文题目','会议名称'])
+    for x in newreference3:
+        #遇到缺失值时，使用空字符串填充
+        writer.writerow([x[0] if len(x)>1 else NULL,x[1] if len(x)>1 else NULL])
+        #writer.writerow([x[0] , x[1]])
+        
+
+
+
 
 '''
 提取列表中每一项中的论文题目以及会议，写入csv文件
-'''
+
 import csv
 with open(r"D:\研究生-心理与行为大数据\ziqianの创造力\ziqian_python\reference.csv",'w',newline='',encoding='utf-8') as f:
         writer=csv.writer(f)
         writer.writerow(['论文题目','会议名称'])
         for x in newreference2:
-                writer.writerow(x[1:3])
+                #遇到缺失值时，使用空字符串填充
+                writer.writerow([x[1] if len(x)>1 else 'null',x[2] if len(x)>2 else 'null'])
 
-
-
-
+'''
